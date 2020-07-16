46e7977ecc966f07c60466867d3ef6a19b7233c7
Jecta <64068518+Jecta@users.noreply.github.com>
2020-07-16 20:07:18 +0200

    Prefix sys test

---
+++ commands/general/prefix.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+    if(!args[0]){
+        let msg = new Discord.MessageEmbed()
+        .setDescription(`The prefix in this server is ***${prefix}***`)
+        .setColor('YELLOW')
+        message.channel.send(msg);
+    }else if(args[0] == "change"){
+        if(!args[1]){
+            let msg3 = new Discord.MessageEmbed()
+            .setDescription("Please specify a ***prefix***")
+            .setColor('YELLOW')
+            return message.channel.send(msg3)
+        }else{
+            db.set(`prefix_${message.guild.id}`, args[1])
+            let msg2 = new Discord.MessageEmbed()
+            .setDescription(`Set bot prefix to ***${args[1]}***`)
+            .setColor('YELLOW')
+            message.channel.send(msg2)
+        }
+    }
+}
--- events/message.js
+++ events/message.js
@@ -1,6 +1,15 @@
+const db = require('quick.db');
 module.exports = async (client, message) => {
 
     if (message.author.bot) return;
+
+    let prefix;
+    if(message.guild){
+        if(message.guild.id != undefined || message.guild.id != null){
+            prefix = db.get(`prefix_${message.guild.id}`)
+        }
+    }
+    if(prefix === null) prefix = default_prefix;
   
     if (message.content.indexOf(client.config.prefix) !== 0) return;
   
