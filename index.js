35a2d7941ad7df3cb1c5ec0bc9d291e002aa3461
Jecta <64068518+Jecta@users.noreply.github.com>
2020-07-16 20:27:48 +0200

    Back to normal

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ client.on("message", message => {
       userInfo.xp = 0
       message.reply(leveled)
   }
-  const args = message.content.slice(config.default_prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
   if(cmd === "level") {
       let userInfo = db[message.author.id];
