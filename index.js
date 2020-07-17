bf636481c132e3caa132720d4333831bd7cf938e
Jecta <64068518+Jecta@users.noreply.github.com>
2020-07-17 13:14:37 +0200

    New handler update

--- index.js
+++ index.js
@@ -16,50 +16,49 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-client.general = new Discord.Collection();
+// Creating command and aliases collection.
+["commands", "aliases"].forEach(x => bot[x] = new Collection());
 
-const commandFiles = fs.readdirSync('./commands/general').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/general/${file}`);
+// A function to load all the commands.
+const load = (dir = "./commands/") => {
 
-    client.commands.set(command.name, command);
-}
+	readdirSync(dir).forEach(dirs => {
+	// we read the commands directory for sub folders and filter the files with name with extension .js
+		const commands = readdirSync(`${dir}${sep}${dirs}${sep}`).filter(files => files.endsWith(".js"));
 
-client.moderation = new Discord.Collection();
+		// we use for loop in order to get all the commands in sub directory
+		for (const file of commands) {
+		// We make a pull to that file so we can add it the bot.commands collection
+			const pull = require(`${dir}/${dirs}/${file}`);
+			// we check here if the command name or command category is a string or not or check if they exist
+			if (pull.help && typeof (pull.help.name) === "string" && typeof (pull.help.category) === "string") {
+				if (bot.commands.get(pull.help.name)) return console.warn(`${warning} Two or more commands have the same name ${pull.help.name}.`);
+				// we add the the comamnd to the collection, Map.prototype.set() for more info
+				bot.commands.set(pull.help.name, pull);
+				// we log if the command was loaded.
+				console.log(`${success} Loaded command ${pull.help.name}.`);
 
-const commandFiles = fs.readdirSync('./commands/moderation').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/moderation/${file}`);
+			}
+			else {
+			// we check if the command is loaded else throw a error saying there was command it didn't load
+				console.log(`${error} Error loading command in ${dir}${dirs}. you have a missing help.name or help.name is not a string. or you have a missing help.category or help.category is not a string`);
+				// we use continue to load other commands or else it will stop here
+				continue;
+			}
+			// we check if the command has aliases, is so we add it to the collection
+			if (pull.help.aliases && typeof (pull.help.aliases) === "object") {
+				pull.help.aliases.forEach(alias => {
+					// we check if there is a conflict with any other aliases which have same name
+					if (bot.aliases.get(alias)) return console.warn(`${warning} Two commands or more commands have the same aliases ${alias}`);
+					bot.aliases.set(alias, pull.help.name);
+				});
+			}
+		}
 
-    client.commands.set(command.name, command);
-}
+	});
+};
 
-client.fun = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands/fun').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/fun/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-client.social = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands/social').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/social/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-client.misc = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands/misc').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/misc/${file}`);
-
-    client.commands.set(command.name, command);
-}
+load();
 
 
 client.login(process.env.token)
