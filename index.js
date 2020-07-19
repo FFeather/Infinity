0610914a3841beb61383a16a36b8fe0bc8aeba33
JectaR!CH <68130184+JectaRCH@users.noreply.github.com>
2020-07-19 12:52:09 +0200

    Updated handler 2

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
 
 
 client.login(process.env.token)
